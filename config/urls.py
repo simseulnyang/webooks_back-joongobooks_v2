62a46139e4da9b0caf606ec3ccf1fb84dd2ea334
simseulnyang <happysseul627@gmail.com>
2025-11-19 20:24:58 +0900

    ✨Feat : swagger 사용을 위한 URL 작성 #3

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users/', include("accounts.urls")),
+    
+    # OpenAPI 스키마(JSON)
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Swagger UI
+    path(
+        "api/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    # ReDoc UI
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
