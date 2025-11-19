b2eb2874bcaec074245720411748ec38c8e77a09
simseulnyang <happysseul627@gmail.com>
2025-11-19 20:25:59 +0900

    ✨Feat : 소셜로그인 사용자 인증 및 권한 조회를 위한 model 설정 #3

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,42 @@
+from django.conf import settings
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+from accounts.managers import UserManager
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=False)
+    profile_image = models.URLField(max_length=200, blank=True)
+    
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.username
+    
+    
+class SocialAccount(models.Model):
+    class Provider(models.TextChoices):
+        KAKAO = "kakao", "Kakao"
+        GOOGLE = "google", "Google"
+        
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="social_accounts")
+    provider = models.CharField(max_length=20, choices=Provider.choices)
+    provider_user_oid = models.CharField(max_length=255, help_text="카카오_oid, 구글_sub 등 소셜 서비스 고유 ID")
+
+    class Meta:
+        unique_together = ("provider", "provider_user_oid")
+
+    def __str__(self):
+        return f"{self.provider} - {self.provider_user_oid}"
