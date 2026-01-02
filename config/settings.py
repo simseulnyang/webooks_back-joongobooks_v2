d2df722306324e789f936e2ed57697c912d6e5d1
simseulnyang <happysseul627@gmail.com>
2026-01-02 20:37:30 +0900

    ✨Feat : 미디어 사용을 위한 settings 설정, 개발 환경에서 media 파일 서빙위한 urlpatterns 작성 및 media폴더 gitignore 등록

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     
     # apps
     'accounts',
+    'books',
 ]
 
 AUTH_USER_MODEL = "accounts.User"
@@ -138,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
