ab3a2d7632c945cb5547152df5205f5ca8c3c1e0
simseulnyang <happysseul627@gmail.com>
2026-01-04 21:57:25 +0900

    ✨Feat :전역 적용을 위한 pagination 및 permission 설정

--- config/settings.py
+++ config/settings.py
@@ -157,7 +157,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework_simplejwt.authentication.JWTAuthentication',),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
