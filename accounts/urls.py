9fc2c45426b015c88a2cc708ad58b158ffc59f64
simseulnyang <happysseul627@gmail.com>
2026-01-28 11:03:30 +0900

    ✨Feat : 토큰 갱신 API view 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
 
 from accounts.views import (
@@ -16,4 +17,6 @@ urlpatterns = [
     path("update/", UserUpdateAPIView.as_view(), name="user_update"),
     path("logout/", LogoutAPIView.as_view(), name="logout"),
     path("delete/", DeleteAccountAPIView.as_view(), name="delete_account"),
+    
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
